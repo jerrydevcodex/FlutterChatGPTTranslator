700fc201c67a0cab774c1c7cf96da240e0854fcd
jerry <jerrynicework@gmail.com>
2024-07-23 10:58:54 +0900

    240723 Apply SafeArea

--- flutter_local/lib/main.dart
+++ flutter_local/lib/main.dart
@@ -5,7 +5,5 @@ import 'package:flutter_local/screen/home_screen.dart';
 
 // Flutter 애플리케이션의 시작 지점
 void main() {
-  // runApp 함수는 Flutter 프레임워크에서 애플리케이션을 시작하는 역할
-  // 여기서는 HomeScreen 위젯을 루트 위젯으로 설정하여 앱을 시작
-  runApp(const HomeScreen());
+  runApp(MaterialApp(home: HomeScreen()));
 }
--- flutter_local/lib/screen/basic_screen.dart
+++ flutter_local/lib/screen/basic_screen.dart
@@ -1,19 +1,6 @@
 import 'package:flutter/material.dart';
 
 class BasicScreen extends StatefulWidget {
-  const BasicScreen({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
   State<BasicScreen> createState() => _BasicScreenState();
 }
@@ -48,7 +35,7 @@ class _BasicScreenState extends State<BasicScreen> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("demo"),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
