1984cd68abd9e0a965bef31273a60da52dc5e1ea
jerry <jerrynicework@gmail.com>
2024-07-24 11:18:26 +0900

    240724 correspondence test

--- flutter_local/lib/screen/basic_screen.dart
+++ flutter_local/lib/screen/basic_screen.dart
@@ -1,5 +1,7 @@
 import 'package:dash_chat_2/dash_chat_2.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class BasicScreen extends StatefulWidget {
   @override
@@ -38,7 +40,7 @@ class _BasicState extends State<BasicScreen> {
           setState(() {
             messages.insert(0, m);
           });
-          Futrue<String> data = sendMessageToServer(m.text);
+          Future<String> data = sendMessageToServer(m.text);
           data.then((value) {
             setState(() {
               messages.insert(
@@ -56,5 +58,38 @@ class _BasicState extends State<BasicScreen> {
     );
   }
 
-  Future<String> sendMessageToServer(String message) async {}
+  Future<String> sendMessageToServer(String message) async {
+    var headers = {
+      'Content-Type': 'application/json',
+      'Authorization':
+          'Bearer sk-proj-VQN6jt3ybHJZhpuSAi6xT3BlbkFJjYo0DmTM4rb5y8IaRSY6',
+    };
+    var request = http.Request(
+        'POST', Uri.parse('https://api.openai.com/v1/chat/completions'));
+    request.body = json.encode({
+      "model": "gpt-4o-mini",
+      "messages": [
+        {
+          "role": "user",
+          "content": message,
+        }
+      ]
+    });
+    request.headers.addAll(headers);
+
+    http.StreamedResponse response = await request.send();
+
+    if (response.statusCode == 200) {
+      String responseString = await response.stream.bytesToString();
+      Map<String, dynamic> jsonResponse = json.decode(responseString);
+      String result = jsonResponse['choices'] != null
+          ? jsonResponse['choices'][0]['message']['content']
+          : "No result found";
+      print(responseString);
+      return result;
+    } else {
+      print(response.reasonPhrase);
+      return "ERROR";
+    }
+  }
 }
